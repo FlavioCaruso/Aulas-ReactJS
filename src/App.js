109c3225614a7c22a64dd0c7b07ae9f1a2838315
Flavio Berbel Caruso <flavio.caruso@butantan.gov.br>
2019-06-28 10:37:37 -0300

    Buscando produtos da API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
+//Importando o CSS
 import './styles.css'
 
+//Importando os Componentes
 import Header from './components/Header'
-
+import Main from './pages/main'
 
 const App = () => (
   <div className="App">
       <Header/>
+      <Main/>
     </div>
 );
 
